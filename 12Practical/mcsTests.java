7ad96e8ba2dd24865e115dc8d5e40a848c9fd04c
LE Davids <159734376+MeowMeowTheCow@users.noreply.github.com>
2026-02-16 15:21:08 +0200

    Add mcsOn3 and mcsOn2A operation counters
    Rename class Main to mcsTests, add an unused java.util.Random import, and introduce two static methods: mcsOn3 and mcsOn2A. Both implement brute-force (cubic-like nested loops) and improved quadratic MCS algorithms respectively, taking an int[] X and returning a long count of the inner addition operations; they also compute a max sum internally but only return the operation count.

--- 12Practical/mcsTests.java
+++ 12Practical/mcsTests.java
@@ -1,3 +1,53 @@
-public class Main {
-    
+import java.util.Random;
+public class mcsTests {
+
+     public static long mcsOn3(int[] X) {
+        int n = X.length;
+        long count = 0;
+        int max = 0;
+        
+        for (int low = 0; low < n; low++) {
+
+            for (int high = low; high < n; high++) {
+                int sum = 0;
+
+                for (int r = low; r < high; r++) {
+                    sum += X[r];
+                    count++;
+                }
+
+                if (sum > max) {
+                    max = sum;
+                }
+
+            }
+
+        }
+
+        return count;
+
+     }
+     public static long mcsOn2A(int[] X) {
+        int n = X.length;
+        long count = 0;
+        int max = 0;
+
+        for (int low = 0; low < n; low++) {
+            int sum = 0;
+
+            for (int r = low; r < n; r++) {
+                sum += X[r];
+                count++;
+
+                if (sum > max) {
+                    max = sum;
+
+                }
+
+            }
+
+        }
+        return count;
+
+    }
 }
