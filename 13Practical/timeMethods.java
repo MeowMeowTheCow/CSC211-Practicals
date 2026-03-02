6b6e370392e7b9535b3cbd51948e5aa51762f8d0
LE Davids <159734376+MeowMeowTheCow@users.noreply.github.com>
2026-03-02 14:17:49 +0200

    Remove unused DecimalFormat, add 14Practical
    Add new 14Practical brief: a PDF (14Practical.pdf) and three Java files (chainedHash.java, hashShuffle.java, openHash.java) for the 14th practical.

--- 13Practical/timeMethods.java
+++ 13Practical/timeMethods.java
@@ -18,7 +18,6 @@ public class timeMethods{
 public static void main(String args[])throws IOException {
    loadData("ulysses.numbered");
 
-   DecimalFormat twoD = new DecimalFormat("0.00");
    DecimalFormat fourD = new DecimalFormat("0.0000");
    DecimalFormat fiveD = new DecimalFormat("0.00000");
 
